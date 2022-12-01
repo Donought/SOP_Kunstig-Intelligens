1ad904aa065ad85c4eebe031a357730db8e0ee71
Donought <114650357+Donought@users.noreply.github.com>
2022-12-01 20:59:59 +0100

    Made the ai's fight each other, although only once

--- sketch.js
+++ sketch.js
@@ -110,20 +110,29 @@ class Menace {
 
 let root = 3; // Root of the amount of fields in the grid
 let gap = 15; // Weird number used to determine spacing of squares
+let bw; // Board width
+let bh; // Board height
 
-let menace = new Menace();
+let menace1 = new Menace();
+let menace2 = new Menace();
 
 let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function setup() {
-  createCanvas(600, 600);
-  console.log(menace.ai[0][0][0][0][0][0][0][0][0]);
+  createCanvas(1000, 600);
+  bw = height;
+  bh = height;
+
+  aiGame(menace1, menace2);
+
+  //console.log(menace1.ai[0][0][0][0][0][0][0][0][0]);
 }
 
 function draw() {
   background(150);
   drawGrid();
 
+  // Draw all pieces on the board
   board.forEach((value, index) => {
     if (value == 1) {
       oPiece(index);
@@ -132,6 +141,16 @@ function draw() {
       xPiece(index);
     }
   });
+
+  rectMode(CORNER);
+  noStroke();
+  fill(50);
+  rect(
+    bw + height / 20,
+    height / 20,
+    width - bw - (height / 20) * 2,
+    (height / 20) * 18
+  );
 }
 
 function drawGrid() {
@@ -141,10 +160,10 @@ function drawGrid() {
   for (let i = 0; i < root; i++) {
     for (let j = 0; j < root; j++) {
       rect(
-        (width / root) * (j + 1) - width / (root * 2),
-        (height / root) * (i + 1) - height / (root * 2),
-        width / root - width / (gap * root),
-        height / root - height / (gap * root)
+        (bw / root) * (j + 1) - bw / (root * 2),
+        (bh / root) * (i + 1) - bh / (root * 2),
+        bw / root - bw / (gap * root),
+        bh / root - bh / (gap * root)
       );
     }
   }
@@ -152,32 +171,32 @@ function drawGrid() {
 
 function xPiece(pos) {
   let coord = convertToCoord(pos);
-  let x = coord[0] * (width / root);
-  let y = coord[1] * (height / root);
-  let thick = ((15 * 3) / root / 700) * ((width + height) / 2); // Weird equation I use to find appropiate thickness
+  let x = coord[0] * (bw / root);
+  let y = coord[1] * (bh / root);
+  let thick = ((15 * 3) / root / 700) * ((bw + bh) / 2); // Weird equation I use to find appropiate thickness
   stroke(255); // Color
   strokeWeight(thick);
 
   // The lines that make up the cross scale with everything else, and as a result, this is pretty unreadable
   line(
-    x - width / root + width / (gap * root) + thick / 2,
-    y - height / root + height / (gap * root) + thick / 2,
-    x - width / (gap * root) - thick / 2,
-    y - height / (gap * root) - thick / 2
+    x - bw / root + bw / (gap * root) + thick / 2,
+    y - bh / root + bh / (gap * root) + thick / 2,
+    x - bw / (gap * root) - thick / 2,
+    y - bh / (gap * root) - thick / 2
   );
   line(
-    x - width / root + width / (gap * root) + thick / 2,
-    y - height / (gap * root) - thick / 2,
-    x - width / (gap * root) - thick / 2,
-    y - height / root + height / (gap * root) + thick / 2
+    x - bw / root + bw / (gap * root) + thick / 2,
+    y - bh / (gap * root) - thick / 2,
+    x - bw / (gap * root) - thick / 2,
+    y - bh / root + bh / (gap * root) + thick / 2
   );
 }
 
 function oPiece(pos) {
   let coord = convertToCoord(pos);
-  let x = coord[0] * (width / root);
-  let y = coord[1] * (height / root);
-  let thick = ((15 * 3) / root / 700) * ((width + height) / 2); // Same equation as I want identical thickness
+  let x = coord[0] * (bw / root);
+  let y = coord[1] * (bh / root);
+  let thick = ((15 * 3) / root / 700) * ((bw + bh) / 2); // Same equation as I want identical thickness
   stroke(255); // Color
   strokeWeight(thick);
   noFill();
@@ -185,10 +204,10 @@ function oPiece(pos) {
 
   // Scales with everything else
   ellipse(
-    x - width / root + width / (gap * root) + thick / 2,
-    y - height / root + height / (gap * root) + thick / 2,
-    x - width / (gap * root) - thick / 2,
-    y - height / (gap * root) - thick / 2
+    x - bw / root + bw / (gap * root) + thick / 2,
+    y - bh / root + bh / (gap * root) + thick / 2,
+    x - bw / (gap * root) - thick / 2,
+    y - bh / (gap * root) - thick / 2
   );
 }
 
@@ -198,6 +217,57 @@ function convertToCoord(pos) {
   let y = Math.ceil(p / root);
   let x = p - (y - 1) * root;
   return [x, y];
+}
+
+function aiGame(ai1, ai2) {
+  // ai1 plays when true, ai2 plays when false
+  let turn = random(0, 1) < 0.5;
+
+  for (let i = 0; i < 9; i++) {
+    if (turn == true) {
+      aiMove(ai1, ai2);
+      turn = !turn;
+    } else {
+      aiMove(ai2, ai1);
+      turn = !turn;
+    }
+  }
+
+  //while (true) {
+  //if (turn == true) {
+  /*let b = board;
+  let t = ai1.ai[b[0]][b[1]][b[2]][b[3]][b[4]][b[5]][b[6]][b[7]][b[8]]; // Temp holder for this array
+
+  ai1.boxes.push(b);
+
+  let move = Math.round(random(0, t.length - 1));
+
+  if (ai1.moves.length == ai2.moves.length) {
+    board[t[move]] = 1;
+  } else {
+    board[t[move]] = 2;
+  }
+
+  ai1.moves.push(move);*/
+  //}
+  //}
+}
+
+function aiMove(ai1, ai2) {
+  let b = board;
+  let t = ai1.ai[b[0]][b[1]][b[2]][b[3]][b[4]][b[5]][b[6]][b[7]][b[8]]; // Temp holder for this array
+
+  ai1.boxes.push(b);
+
+  let move = Math.round(random(0, t.length - 1));
+
+  if (ai1.moves.length == ai2.moves.length) {
+    board[t[move]] = 1;
+  } else {
+    board[t[move]] = 2;
+  }
+
+  ai1.moves.push(move);
 }
 
 // Has to do with chrome's rules. It gives me an error if I don't do this, the error doesn't stop the program from working though
