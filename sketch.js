1dc38f5aee25484439a149eb3e1a78711b597884
Donought <114650357+Donought@users.noreply.github.com>
2022-12-01 18:21:49 +0100

    Visuals added
    With inspiration from my previous tic tac toe creation, I made a tic tac toe board with x and o pieces. I also added a function to change 1d positions into 2d positions, as 2d positions are easier to use for visuals. As of this commit, the program has a board variable, in which I can put any combination of pieces, and I can then display that exact board.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,22 @@
 class Menace {
   constructor() {
-    this.ai = [[], [], []];
-
+    this.ai = [];
     this.marbles = 2;
+    this.restart();
 
+    this.pos = [];
+    this.moves = [];
+    this.boxes = [];
+  }
+
+  clear() {
+    this.pos = [];
+    this.moves = [];
+    this.boxes = [];
+  }
+
+  restart() {
+    this.ai = [[], [], []];
     for (let i = 0; i < 3; i++) {
       this.ai[i] = [[], [], []];
       for (let j = 0; j < 3; j++) {
@@ -95,13 +108,99 @@ class Menace {
   }
 }
 
+let root = 3; // Root of the amount of fields in the grid
+let gap = 15; // Weird number used to determine spacing of squares
+
 let menace = new Menace();
 
+let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
   console.log(menace.ai[0][0][0][0][0][0][0][0][0]);
 }
 
 function draw() {
-  background(220);
+  background(150);
+  drawGrid();
+
+  board.forEach((value, index) => {
+    if (value == 1) {
+      oPiece(index);
+    }
+    if (value == 2) {
+      xPiece(index);
+    }
+  });
+}
+
+function drawGrid() {
+  rectMode(CENTER);
+  noStroke();
+  fill(50);
+  for (let i = 0; i < root; i++) {
+    for (let j = 0; j < root; j++) {
+      rect(
+        (width / root) * (j + 1) - width / (root * 2),
+        (height / root) * (i + 1) - height / (root * 2),
+        width / root - width / (gap * root),
+        height / root - height / (gap * root)
+      );
+    }
+  }
+}
+
+function xPiece(pos) {
+  let coord = convertToCoord(pos);
+  let x = coord[0] * (width / root);
+  let y = coord[1] * (height / root);
+  let thick = ((15 * 3) / root / 700) * ((width + height) / 2); // Weird equation I use to find appropiate thickness
+  stroke(255); // Color
+  strokeWeight(thick);
+
+  // The lines that make up the cross scale with everything else, and as a result, this is pretty unreadable
+  line(
+    x - width / root + width / (gap * root) + thick / 2,
+    y - height / root + height / (gap * root) + thick / 2,
+    x - width / (gap * root) - thick / 2,
+    y - height / (gap * root) - thick / 2
+  );
+  line(
+    x - width / root + width / (gap * root) + thick / 2,
+    y - height / (gap * root) - thick / 2,
+    x - width / (gap * root) - thick / 2,
+    y - height / root + height / (gap * root) + thick / 2
+  );
+}
+
+function oPiece(pos) {
+  let coord = convertToCoord(pos);
+  let x = coord[0] * (width / root);
+  let y = coord[1] * (height / root);
+  let thick = ((15 * 3) / root / 700) * ((width + height) / 2); // Same equation as I want identical thickness
+  stroke(255); // Color
+  strokeWeight(thick);
+  noFill();
+  ellipseMode(CORNERS); // This line is important if you want to dechipher the following parameters
+
+  // Scales with everything else
+  ellipse(
+    x - width / root + width / (gap * root) + thick / 2,
+    y - height / root + height / (gap * root) + thick / 2,
+    x - width / (gap * root) - thick / 2,
+    y - height / (gap * root) - thick / 2
+  );
+}
+
+// Convert 1d pos to 2d pos
+function convertToCoord(pos) {
+  p = pos + 1;
+  let y = Math.ceil(p / root);
+  let x = p - (y - 1) * root;
+  return [x, y];
+}
+
+// Has to do with chrome's rules. It gives me an error if I don't do this, the error doesn't stop the program from working though
+function touchStarted() {
+  getAudioContext().resume();
 }
